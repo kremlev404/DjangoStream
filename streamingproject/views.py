60fb69bd10f7062204272c9764a30bb09dddc21d
kremlev404 <kremlev404@gmail.com>
2019-12-12 16:51:28 +0300

    v1

---
+++ streamingproject/views.py
@@ -0,0 +1,130 @@
+from django.shortcuts import render
+from django.http import HttpResponse,StreamingHttpResponse, HttpResponseServerError,HttpResponseRedirect
+from django.views.decorators import gzip
+
+from imutils.video import VideoStream
+from imutils.video import FPS
+
+import cv2
+from time import time
+import imutils
+from math import sqrt
+import numpy as np
+
+ID = 1
+renet = cv2.dnn.readNet('person-reidentification-retail-0079/FP32/person-reidentification-retail-0079.xml',
+                            'person-reidentification-retail-0079/FP32/person-reidentification-retail-0079.bin')
+net = cv2.dnn.readNet('person-detection-retail-0013/FP32/person-detection-retail-0013.xml',
+                          'person-detection-retail-0013/FP32/person-detection-retail-0013.bin')   
+netsize = (544, 320)
+renetsize = (64, 160) 
+
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+renet.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
+renet.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)   
+#___________________________________VIDEO NAME/CHANGE TO 0 to get stream FROM WEBCAM
+video = cv2.VideoCapture('2.mp4') 
+
+distance = 0    
+chips = []   
+times = {}
+data = {}
+fps = 1
+trigger = 0.8
+dist_trigger = 0.3   
+f_trigger = 0.5
+
+def compare(data, chip, save=0):
+    reblob = cv2.dnn.blobFromImage(chip, size=renetsize, ddepth=cv2.CV_8U)
+    renet.setInput(reblob)
+    reout = renet.forward()
+    reout = reout.reshape(256)
+    reout /= sqrt(np.dot(reout, reout))
+    ide = 1
+    distance = -1
+
+    if len(data) != 0:
+        for x in data:
+            distance = np.dot(reout, data[x])
+            ide += 1
+            if distance > dist_trigger:
+                ide = x
+                break
+
+    if distance < dist_trigger:
+        data['id{}'.format(ide)] = reout
+        if save:
+            cv2.imwrite('photos/id{}.jpg'.format(ide), chip)
+
+    return distance, ide
+      
+class VideoCamera(object):
+    
+    def get_frame(self):
+        start = time()
+        grab, frame = video.read()
+        if not grab:
+            raise Exception('Image not found!')
+
+        blob = cv2.dnn.blobFromImage(frame, size=netsize, ddepth=cv2.CV_8U)
+        net.setInput(blob)
+        out = net.forward()
+
+        objects = 0
+        for detection in out.reshape(-1, 7):
+            confidence = float(detection[2])
+            xmin = int(detection[3] * frame.shape[1])
+            ymin = int(detection[4] * frame.shape[0])
+            xmax = int(detection[5] * frame.shape[1])
+            ymax = int(detection[6] * frame.shape[0])
+
+            if confidence > trigger:
+                objects += 1
+                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+                chip = frame[ymin:ymax, xmin:xmax]                
+                try:               
+                    distance, ID = compare(data, chip, 1)
+                except:
+                    continue
+
+                cv2.putText(frame, '{}'.format(ID), (xmin, ymax - 5),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+                
+        end = time()
+        fps = 1 / (end - start)
+
+        cv2.putText(frame, 'fps:{:.2f}'.format(fps), (5, 25),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(frame, 'objects:{}'.format(objects), (5, frame.shape[0]-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(frame, 'IDs:{}'.format(len(data)), (5, frame.shape[0]-45),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        jpeg = cv2.imencode('.jpg',frame)[1].tostring()
+        '''print(jpeg)
+        j = jpeg.tobytes()
+        print(j)'''
+        return jpeg
+
+def gen(camera):
+    while True:
+        frame_1 = VideoCamera().get_frame()
+        yield(b'--frame_1\r\n'
+        b'Content-Type: image/jpeg\r\n\r\n' + frame_1 + b'\r\n\r\n')
+        
+
+def indexscreen(request): 
+    try:
+        template = "screens.html"
+        return render(request,template)
+
+    except HttpResponseServerError:
+        print("aborted")
+
+@gzip.gzip_page
+
+def dynamic_stream(request,num=0,stream_path="0"):
+    stream_path = 'add your camera stream here that can rtsp or http'
+    return StreamingHttpResponse(gen(VideoCamera()),content_type="multipart/x-mixed-replace;boundary=frame")
+
