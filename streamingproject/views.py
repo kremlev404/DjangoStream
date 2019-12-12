8e333e7debd470621f6cf23421971f6df9e3dcc5
kremlev404 <kremlev404@gmail.com>
2019-12-12 22:28:12 +0300

    H_estimation

--- streamingproject/views.py
+++ streamingproject/views.py
@@ -11,18 +11,34 @@ import imutils
 from math import sqrt
 import numpy as np
 
+BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+               "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+               "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+               "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
+
+POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
+               ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"]]
+
 ID = 1
 renet = cv2.dnn.readNet('person-reidentification-retail-0079/FP32/person-reidentification-retail-0079.xml',
                             'person-reidentification-retail-0079/FP32/person-reidentification-retail-0079.bin')
 net = cv2.dnn.readNet('person-detection-retail-0013/FP32/person-detection-retail-0013.xml',
-                          'person-detection-retail-0013/FP32/person-detection-retail-0013.bin')   
+                          'person-detection-retail-0013/FP32/person-detection-retail-0013.bin')
+est_net = cv2.dnn.readNet('human-pose-estimation-0001/FP32/human-pose-estimation-0001.xml',
+                          'human-pose-estimation-0001/FP32/human-pose-estimation-0001.bin')                            
 netsize = (544, 320)
 renetsize = (64, 160) 
+estnetsize = (456, 256)
 
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 renet.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
-renet.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)   
+renet.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)  
+est_net.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
+est_net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU) 
 #___________________________________VIDEO NAME/CHANGE TO 0 to get stream FROM WEBCAM
 video = cv2.VideoCapture('2.mp4') 
 
@@ -34,6 +50,7 @@ fps = 1
 trigger = 0.8
 dist_trigger = 0.3   
 f_trigger = 0.5
+crop_frame = video.read()
 
 def compare(data, chip, save=0):
     reblob = cv2.dnn.blobFromImage(chip, size=renetsize, ddepth=cv2.CV_8U)
@@ -90,7 +107,50 @@ class VideoCamera(object):
 
                 cv2.putText(frame, '{}'.format(ID), (xmin, ymax - 5),
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
-                
+#_____________________________________________________________________________________
+#_____________HUMAN_POSE_ESTIMATION___________________________________________________
+#_____________________________________________________________________________________
+#_____________________________________________________________________________________
+        crop_frame = frame[ymin:ymax, xmin:xmax]
+        frameWidth = frame.shape[1]
+        frameHeight = frame.shape[0]
+
+        est_blob = cv2.dnn.blobFromImage(frame, size=estnetsize, ddepth=cv2.CV_8U)
+        est_net.setInput(est_blob)
+        est_out = est_net.forward()
+        est_out = est_out[:, :19, :, :]
+        assert(len(BODY_PARTS) == est_out.shape[1])
+        points = []
+
+        for i in range(len(BODY_PARTS)):
+            heatMap = est_out[0, i, :, :]
+            _, conf, _, point = cv2.minMaxLoc(heatMap)
+            x = (frameWidth * point[0]) / est_out.shape[3]
+            y = (frameHeight * point[1]) / est_out.shape[2]
+            points.append((int(x), int(y)) if conf > 0.2 else None)
+
+        for pair in POSE_PAIRS:
+            partFrom = pair[0]
+            partTo = pair[1]
+            assert(partFrom in BODY_PARTS)
+            assert(partTo in BODY_PARTS)
+
+            idFrom = BODY_PARTS[partFrom]
+            idTo = BODY_PARTS[partTo]
+
+            if points[idFrom] and points[idTo]:
+                cv2.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
+                cv2.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv2.FILLED)
+                cv2.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv2.FILLED)
+
+        t, _ = est_net.getPerfProfile()
+        freq = cv2.getTickFrequency() / 1000
+        cv2.putText(frame, '%.2fms' % (t / freq), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+#_____________________________________________________________________________________
+#_____________HUMAN_POSE_ESTIMATION(DONE)_____________________________________________
+#_____________________________________________________________________________________
+#_____________________________________________________________________________________
+
         end = time()
         fps = 1 / (end - start)
 
